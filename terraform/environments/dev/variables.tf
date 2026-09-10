652db0a64c364ad9282966e3c52f1b99c458dafc
immuali <immusaleh@gmail.com>
2026-09-10 14:25:15 +0300

    Provision modular AWS network infrastructure with Terraform

---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  description = "AWS region used for the project"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "automated-cloud-deployment"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+}
